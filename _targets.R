b04f95eb0203eb0896f6e0cacc80281ec5d7f6ed
cmarshall178 <ccm243@cornell.edu>
2026-04-17 17:47:47 -0400

    adjusting target file with naming changes

--- _targets.R
+++ _targets.R
@@ -1,13 +1,12 @@
-#install.packages("targets")
 library(targets)
 library(tarchetypes)
 library(here)
 
-source(here::here("RCode/import.R"))
-source(here::here("RCode/clean.R"))
-source(here::here("RCode/qa_checks.R"))
-source(here::here("RCode/summarize.R"))
-source(here::here("RCode/utils.R"))
+source(here::here("R", "import.R"))
+source(here::here("R", "clean.R"))
+source(here::here("R", "qa_checks.R"))
+source(here::here("R", "summarize.R"))
+source(here::here("R", "utils.R"))
 
 tar_option_set(
   packages = c(
@@ -16,15 +15,14 @@ tar_option_set(
   )
 )
 
-
 list(
   tar_target(project_dirs, ensure_project_dirs(), cue = tar_cue(mode = "always")),
-
-  tar_target(raw_files, find_raw_files(include_examples = TRUE)),
-  tar_target(raw_data, read_all_zoop(include_examples = TRUE)),
+  
+  tar_target(raw_files, find_raw_files(include_examples = FALSE)),
+  tar_target(raw_data, read_all_zoop(include_examples = FALSE)),
   tar_target(clean_data, clean_zoop(raw_data)),
   tar_target(compiled_csv, write_compiled_data(clean_data), format = "file"),
-
+  
   tar_target(missing_required, check_missing_required(clean_data)),
   tar_target(negative_counts, check_negative_counts(clean_data)),
   tar_target(nonpositive_split_factor, check_nonpositive_split_factor(clean_data)),
@@ -32,10 +30,10 @@ list(
   tar_target(duplicate_organism_rows, check_duplicate_organism_rows(clean_data)),
   tar_target(station_standardization, check_station_standardization(clean_data)),
   tar_target(qa_link_issues, check_qa_links(clean_data)),
-
+  
   tar_target(
     qa_summary,
-    make_qa_summary(
+    build_qa_summary(
       missing_required = missing_required,
       negative_counts = negative_counts,
       nonpositive_split_factor = nonpositive_split_factor,
@@ -45,10 +43,11 @@ list(
       qa_link_issues = qa_link_issues
     )
   ),
+  
   tar_target(qa_summary_csv, write_qa_summary(qa_summary), format = "file"),
-
+  
   tar_target(sample_summary, summarize_samples(clean_data)),
   tar_target(sample_summary_csv, write_sample_summary(sample_summary), format = "file"),
-
+  
   tar_render(qa_report, "reports/qa_report.Rmd")
 )
