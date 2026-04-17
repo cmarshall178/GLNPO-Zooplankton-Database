eaa8c3a35101b3b4e336ce2e14327a955b10c580
Chris Marshall <ccm243@cornell.edu>
2026-04-17 11:45:57 -0400

    git hub setup; and removed draft code

--- RCode/Dataframe Import.R
+++ RCode/Dataframe Import.R
@@ -2,6 +2,8 @@
 #here::i_am("GLNPO Zooplankton Database")
 install.packages("usethis") 
 library(usethis)
+usethis::use_git()
+usethis::use_github()
 
 library(tidyverse)
 library(here)
@@ -32,64 +34,3 @@ read_zoop <- function(files) {
 }
 
 zoop_data <- map_dfr(files, read_zoop)
-
-
-# Detect header row dynamically
-detect_header_row <- function(path, sheet = 1, n_max = 20) {
-  preview <- read_excel(path, sheet = sheet, col_names = FALSE, n_max = n_max)
-  
-  header_row <- preview %>%
-    mutate(row_id = row_number()) %>%
-    pivot_longer(-row_id) %>%
-    group_by(row_id) %>%
-    summarise(non_na = sum(!is.na(value))) %>%
-    arrange(desc(non_na)) %>%
-    slice(1) %>%
-    pull(row_id)
-  
-  return(header_row)
-}
-
-# Clean a single Excel file
-load_zoop_excel <- function(path) {
-  
-  header_row <- detect_header_row(path)
-  
-  df <- read_excel(path, skip = header_row - 1) %>%
-    clean_names()
-  
-  # Remove empty / pivot artifact columns
-  df <- df %>%
-    select(where(~ !all(is.na(.)))) %>%
-    select(-matches("^unnamed"))
-  
-  df
-}
-
-# Load all raw files
-load_all_zoop <- function() {
-  files <- list.files(here("Raw Data/GLNPO 2024 Spring/Erie/D20/Rots"), pattern = ".xlsx$", full.names = TRUE)
-  
-  map_dfr(files, load_zoop_excel, .id = "source_file")
-}
-
-
-
-load_raw_data <- function() {
-  read_xls(here("Raw Data/GLNPO 2024 Spring/Erie/D20/Rots/"), col_types = cols())
-}
-
-# 1. Get a vector of file paths
-file_list <- list.files(here(path = "Raw Data/GLNPO 2024 Spring/Erie/D20/Rots", pattern = "*.xlsx", full.names = TRUE))
-
-# 2. Read and combine all files into one dataframe
-combined_data <- file_list %>%
-  set_names() %>% # Keeps filenames as identifiers if needed
-  map_dfr(read_excel, .id = "source_file")
-
-
-
-
-load_taxonomy <- function() {
-  read_csv(here("data/metadata/taxonomy_reference.csv"), col_types = cols())
-}
